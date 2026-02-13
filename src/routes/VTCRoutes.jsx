0aa7e2c422136abc9a696b835c93547aca256f0b
Your Name <you@example.com>
2026-02-13 12:07:09 +0530

    new changes

--- src/routes/VTCRoutes.jsx
+++ src/routes/VTCRoutes.jsx
@@ -3,8 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Layout from '../components/layout/Layout'
 import Login from "../pages/auth/Login"
 import ForgotPassword from '../pages/auth/ForgotPassword'
+
+//student
 import StudentDashboard from "../pages/student/dashboard/StudentDashboard"
+import StudentProfile from "../pages/student/studentprofile/StudentProfile"
+
+
+//lecturer
 import LecturerDashboard from "../pages/lecturer/dashboard/LecturerDashboard"
+import LecturerProfile from "../pages/lecturer/lecturerprofile/LecturerProfile"
+
+import Settings from '../pages/settings/Settings'
+import ContactUs from '../pages/contactus/ContactUs'
 
 function VTCRoutes() {
   return (
@@ -19,10 +29,16 @@ function VTCRoutes() {
         <Route path="/" element={<Layout />}>
           {/* Student Role Routes */}
           <Route path="/studentdashboard" element={<StudentDashboard />} />
+          <Route path="/studentprofile" element={<StudentProfile />} />
           
 
           {/* Lecturer Role Routes */}
           <Route path="/lecturerdashboard" element={<LecturerDashboard />} />
+          <Route path="/lecturerprofile" element={<LecturerProfile />} />
+
+
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/contactus" element={<ContactUs />} />
         </Route>
 
         {/* You can also use nested routes like this: */}
